83f960172c8a380f7ca382d3525859d4ce248c43
Evgeniy Nochkin <nochjo@yandex.ru>
2020-05-25 13:36:31 +0300

    25/05/2020

--- src/main/resources/static/js/edit.js
+++ src/main/resources/static/js/edit.js
@@ -17,37 +17,23 @@ $(document).ready(function () {
 
         console.log(listuser)
 
-
+        //Построение таблицы 3х3 в myTable
         var elem = document.querySelector('#myTable');
-        createTable(elem, 3, 3);
+        var table = document.createElement('table');
+        for (var i = 0; i < 3; i++) {
+                var tr = document.createElement('tr');
+                for (var j = 0; j < 3; j++) {
+                        var td = document.createElement('td');
+                        tr.appendChild(td);
+                }
+                table.appendChild(tr);
+        }
+        elem.appendChild(table);
 
+        //
         var tab = '<table class="table" id="userTable" border="1" cellpadding="5">';
         tab += '<thead><tr>';
         tab += '<th scope="col">ID</th><th scope="col">Логин</th><th scope="col">Имя</th><th scope="col">Действие</th>';
         tab += '</tr></thead></table>';
         return tab;
 });
-
-function tableCreate() {
-        var body = document.getElementsByTagName('body')[0];
-        var tbl = document.createElement('table');
-        tbl.style.width = '100%';
-        tbl.setAttribute('border', '1');
-        var tbdy = document.createElement('tbody');
-        for (var i = 0; i < 3; i++) {
-                var tr = document.createElement('tr');
-                for (var j = 0; j < 2; j++) {
-                        if (i == 2 && j == 1) {
-                                break
-                        } else {
-                                var td = document.createElement('td');
-                                td.appendChild(document.createTextNode('\u0020'))
-                                i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
-                                tr.appendChild(td)
-                        }
-                }
-                tbdy.appendChild(tr);
-        }
-        tbl.appendChild(tbdy);
-        body.appendChild(tbl)
-}
